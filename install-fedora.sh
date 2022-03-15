715adb64182a888413a83055baf46f4a0668fbc6
Unzor <71789103+Unzor@users.noreply.github.com>
2022-03-15 18:50:20 -0500

    Update install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -1,5 +1,5 @@
 # Install dependencies
-sudo dnf install libdrm-devel libslirp-devel mesa-libGLU-devel gtk3-devel libpcap-devel libsamplerate-devel libaio-devel SDL2-devel libepoxy-devel pixman-devel gcc-c++ ninja-build openssl-devel
+sudo dnf install libdrm-devel libslirp-devel mesa-libGLU-devel gtk3-devel libpcap-devel libsamplerate-devel libaio-devel SDL2-devel libepoxy-devel pixman-devel gcc-c++ ninja-build openssl-devel | yes
 
 # Clone and build
 git clone https://github.com/mborgerson/xemu.git
