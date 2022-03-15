975ceb5e5e630deb1917e5c5169f889738ec3c97
Unzor <71789103+Unzor@users.noreply.github.com>
2022-03-15 18:49:21 -0500

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,6 +1,6 @@
 # Install dependencies
 sudo apt update
-sudo apt install git build-essential libsdl2-dev libepoxy-dev libpixman-1-dev libgtk-3-dev libssl-dev libsamplerate0-dev libpcap-dev ninja-build
+sudo apt install git build-essential libsdl2-dev libepoxy-dev libpixman-1-dev libgtk-3-dev libssl-dev libsamplerate0-dev libpcap-dev ninja-build | yes
 
 # Clone and build
 git clone https://github.com/mborgerson/xemu.git
