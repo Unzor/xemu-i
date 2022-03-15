3a215cd467e460fe1ead7c7ce8eb59a67ca2ab1f
Unzor <71789103+Unzor@users.noreply.github.com>
2022-03-15 18:38:48 -0500

    Create install-fedora.sh

---
+++ install-fedora.sh
@@ -0,0 +1,10 @@
+# Install dependencies
+sudo dnf install libdrm-devel libslirp-devel mesa-libGLU-devel gtk3-devel libpcap-devel libsamplerate-devel libaio-devel SDL2-devel libepoxy-devel pixman-devel gcc-c++ ninja-build openssl-devel
+
+# Clone and build
+git clone https://github.com/mborgerson/xemu.git
+cd xemu
+./build.sh
+
+# Run
+./dist/xemu
